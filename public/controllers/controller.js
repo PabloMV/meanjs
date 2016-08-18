8b4e428ec4082bddaa93a59ba2106dc788b46c36
PabloMV <pablo.viana@portotech.org>
2016-08-17 23:12:34 -0300

    yes

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,7 +2,15 @@ var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
     console.log("Hello World from controller");
 
-    $http.get('/contactlist');
+    $http.get('/contactlist').success(function(response){
+        $scope.contactlist = response;
+    });
   
+    $scope.addContact = function(){
+        console.log($scope.contact);
+        $http.post('/contactlist', $scope.contact).success(function (response) {
+          console.log(response);  
+        });
 
+    };
 }]);
--- server.js
+++ server.js
@@ -2,14 +2,26 @@ var express = require('express');
 var app = express();
 var mongo = require('mongojs');
 var db = mongo('contactlist', ['contactlist']);
+var bodyParser = require('body-parser');
 
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+
 app.get('/contactlist', function(req, res){
     db.contactlist.find(function(err, docs){
         console.log(docs);
         res.json(docs);
     });
+});
+
+app.post('/contactlist', function(req, res){
+        req.body;
+        console.log(req.body);
+       db.contactlist.insert(req.body, function(err, doc){
+           res.json(doc);
+       })
+   
 });
 
 app.listen(3000);
